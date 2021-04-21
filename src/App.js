5118404533141bd32a4901645f46e7d968e542c7
CodeBucks <codebucks27@gmail.com>
2021-04-21 13:04:40 +0530

    Final Commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./css/main.css";
+import DisplayTodos from "./components/DisplayTodos";
+import Todos from "./components/Todos";
+
+import { motion } from "framer-motion";
+function App() {
+  return (
+    <div className="App">
+      <motion.h1
+        initial={{ y: -200 }}
+        animate={{ y: 0 }}
+        transition={{ type: "spring", duration: 0.5 }}
+        whileHover={{ scale: 1.1 }}
+      >
+        Todo App
+      </motion.h1>
+      <motion.div
+        initial={{ y: 1000 }}
+        animate={{ y: 0 }}
+        transition={{ type: "spring", duration: 1 }}
+      >
+        <Todos />
+        <DisplayTodos />
+      </motion.div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Todos.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { addTodos } from "../redux/reducer";
+import { GoPlus } from "react-icons/go";
+import { motion } from "framer-motion";
+
+const mapStateToProps = (state) => {
+  return {
+    todos: state,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addTodo: (obj) => dispatch(addTodos(obj)),
+  };
+};
+
+const Todos = (props) => {
+  const [todo, setTodo] = useState("");
+
+  const handleChange = (e) => {
+    setTodo(e.target.value);
+  };
+
+  const add = () => {
+    if (todo === "") {
+      alert("Input is Empty");
+    } else {
+      props.addTodo({
+        id: Math.floor(Math.random() * 1000),
+        item: todo,
+        completed: false,
+      });
+      setTodo("");
+    }
+  };
+  //console.log("props from store", props);
+  return (
+    <div className="addTodos">
+      <input
+        type="text"
+        onChange={(e) => handleChange(e)}
+        className="todo-input"
+        value={todo}
+      />
+
+      <motion.button
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        className="add-btn"
+        onClick={() => add()}
+      >
+        <GoPlus />
+      </motion.button>
+      <br />
+    </div>
+  );
+};
+//we can use connect method to connect this component with redux store
+export default connect(mapStateToProps, mapDispatchToProps)(Todos);
