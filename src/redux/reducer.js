e9b7ba121309b3bbd42feaa69f60178b393dcbf5
Marcin Skowronek <marcinfoto35@op.pl>
2022-03-09 14:50:53 +0100

    Working on actions and reducers

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -7,6 +7,59 @@ if (localStore !== null && localStore === []) {
   localArray.push(localStore);
 }
 
+/* action name creator */
+// const reducerName = 'order';
+// const createActionName = name => `app/${reducerName}/${name}`;
+
+// /* action types */
+// const FETCH_START = createActionName('FETCH_START');
+// const FETCH_SUCCESS = createActionName('FETCH_SUCCESS');
+// const FETCH_ERROR = createActionName('FETCH_ERROR');
+// const ADD_TODOS = createActionName('ADD_TODOS');
+// const REMOVE_TODOS = createActionName('REMOVE_TODOS');
+// const UPDATE_TODOS = createActionName('UPDATE_TODOS');
+// const COMPLETE_TODOS = createActionName('COMPLETE_TODOS');
+// const DELETE_TODOS = createActionName('DELETE_TODOS');
+
+// /* action creators */
+// export const fetchStarted = payload => ({ payload, type: FETCH_START });
+// export const fetchSuccess = payload => ({ payload, type: FETCH_SUCCESS });
+// export const fetchError = payload => ({ payload, type: FETCH_ERROR });
+// export const addTodos = payload => ({ payload, type: ADD_TODOS });
+// export const removeTodos = payload => ({ payload, type: REMOVE_TODOS });
+// export const updateTodos = payload => ({ payload, type: UPDATE_TODOS });
+// export const completeTodos = payload => ({ payload, type: COMPLETE_TODOS });
+// export const deleteTodos = payload => ({ payload, type: DELETE_TODOS });
+
+// /* reducer */
+// export const reducer = (state = [], action = {}) => {
+//   switch (action.type) {
+//     case ADD_TODOS: {
+//       console.log(state);
+//       state.push(action.payload);
+//       localArray.push(action.payload);
+//       localStorage.setItem('todos', JSON.stringify(localArray));
+//       // const addTask = state.find(item => (item.id === action.payload.id) ? 'undefined' : false);
+//       // const id = action.payload.id;
+//       // console.log(action.payload.quantity);
+//       // console.log('inCart1', inCart);
+//       return {
+//         ...state,
+//         // todis: addTask
+//         //   ? state.map(item =>
+//         //     item.id === action.payload.id
+//         //       ? { ...item }
+//         //       : item
+//         //   )
+//         //   : [...state, action.payload],
+//       };
+//     }
+//     default:
+//       return state;
+//   }
+// };
+
+
 const addTodoReducer = createSlice({
   name: "todos",
   initialState,
